3a812a223a3da2272becf006407859b8a2efc9d1
Romanti-Ezer <romanti.santos@fatec.sp.gov.br>
2017-08-29 18:47:03 -0300

    Simplicacao e melhora do codigo

--- index.php
+++ index.php
@@ -7,31 +7,22 @@
     </head>
     <body>
         <h1>Bem-vindo!</h1>
-        <div class="pagina-login">
             <div class="formulario">
-                <form class="form-registro" method="post" action="Cadastrar.php">
+                <form method="post" action="Cadastrar.php">
                     <h2>Cadastrar-se</h2>
                     <input type="text" placeholder="Nome completo" name="nome" required="required"/>
                     <input type="password" placeholder="Senha" name="senha" required="required"/>
                     <input type="text" placeholder="Endereço de e-mail" name="email" required="required"/>
-                    <button>Criar conta</button>
-                
+                    <a href="#"><button>Criar conta</button></a>					
                 </form>
                 
                 <h2>Já possui uma conta?</h2>
                 
-                <form class="login-form" method="post" action="Login.php">
+                <form method="post" action="Login.php">
                     <input type="text" placeholder="E-mail" name="email" required="required"/>
                     <input type="password" placeholder="Senha" name="senha" required="required"/>
-                    <button>Login</button>
-                    
+                    <button>Login</button>      
                 </form>
             </div>
-        </div>
-        
-        
-        <?php
-            //
-        ?>
     </body>
 </html>
